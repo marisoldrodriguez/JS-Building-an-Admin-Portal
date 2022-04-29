fc0e0670820e6d3b97d1a4584780af1257b8b95b
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-04-29 16:57:10 -0400

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,43 @@
-
 // Your Code Here
+
+// Retrieving list of books from the server
+async function main() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let result = await response.json();
+  console.log(result);
+  result.forEach(renderBook);
+}
+
+
+// Display a list of book titles to the admin
+function renderBook(book) {
+  let bookContainer = document.getElementById("root");
+  let li = document.createElement("li");
+  li.textContent = book.title;
+  let quantityInput = document.createElement("input");
+  quantityInput.setAttribute("type", "number");
+  quantityInput.value = book.quatity;
+
+  let saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+
+  // Still need to get the PATCH method to update quantity!!!!
+  saveButton.addEventListener("click", async () => {
+    // let buttonResponse = 
+    await fetch("http://localhost:3001/updateBook", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: book.id,
+        quatity: quantityInput.value,
+      }),
+    });
+    // let buttonResult = await buttonResponse.json();
+    // console.log(buttonResult);
+  });
+
+  li.append(quantityInput, saveButton);
+  bookContainer.append(li);
+}
+
+main();
